f50f11c84068f88f0fd9fafab2344ef2377bcc8c
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2025-10-27 15:03:24 -0300

    feat: Refactor Cards component to use CardLayout for improved structure and readability

--- src/components/catalog/cards/cards.tsx
+++ src/components/catalog/cards/cards.tsx
@@ -1,18 +1,10 @@
-import { Button, ConfigProvider, Radio } from "antd";
-import { useNavigate } from "react-router-dom";
 import { useState, useRef, useEffect } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import CardLayout from "./cardLayout";
 
 export default function Cards() {
-  const navigate = useNavigate();
-  const [expandedCard, setExpandedCard] = useState<string | null>(null);
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const scrollContainerRef = useRef<HTMLDivElement>(null);
-
-  const toggleDetails = (cardType: string) => {
-    setExpandedCard(expandedCard === cardType ? null : cardType);
-  };
 
   const scrollToSlide = (index: number) => {
     if (scrollContainerRef.current) {
@@ -156,175 +148,6 @@ export default function Cards() {
     },
   ];
 
-  const Card = ({ cardData }: { cardData: (typeof cardsData)[0] }) => {
-    const [selectedClientType, setSelectedClientType] =
-      useState<string>("cliente");
-
-    return (
-      <div className="flex flex-col w-full items-center relative">
-        {cardData.badge && (
-          <div
-            className={`${cardData.badgeStyle} absolute rounded-tl-sm rounded-r-sm self-start -top-2 z-10`}
-          >
-            {cardData.badge}
-          </div>
-        )}
-        <div className="flex w-full max-w-[260px] flex-col border border-gray-300 rounded-sm bg-white shadow-sm">
-          <div className=" flex flex-col gap-2">
-            <div className="text-start pt-6 px-4">
-              <p style={{ margin: 0 }} className=" text-[14px] ">
-                {cardData.subtitle}
-              </p>
-              <h3 className="text-[28px]  ">{cardData.title}</h3>
-            </div>
-
-            <ConfigProvider
-              theme={{
-                token: {
-                  colorPrimary: "#660099",
-                },
-              }}
-            >
-              <div className="px-4  flex text-start flex-col gap-2">
-                <Radio.Group
-                  value={selectedClientType}
-                  onChange={(e) => setSelectedClientType(e.target.value)}
-                  className="flex flex-col gap-2"
-                >
-                  <div className="flex items-start">
-                    <Radio
-                      value="cliente"
-                      style={{
-                        color: "#660099",
-                      }}
-                    />
-                    <div className="flex flex-col">
-                      <p
-                        style={{ margin: 0 }}
-                        className="text-gray-700  text-[12px] "
-                      >
-                        {cardData.clientType}
-                      </p>
-                      <p className="text-gray-600 text-[10px]">
-                        {cardData.storage}
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center text-start ">
-                    <Radio
-                      value="nao-cliente"
-                      style={{
-                        color: "#660099",
-                      }}
-                    />
-                    <p
-                      style={{ margin: 0 }}
-                      className="text-gray-700  text-[12px] "
-                    >
-                      {cardData.nonClientType}
-                    </p>
-                  </div>
-                </Radio.Group>
-              </div>
-            </ConfigProvider>
-
-            <div className="mx-4 mb-3 flex flex-col items-start bg-[#f0f0f0] p-3 px-2 text-start rounded text-[14px]">
-              <p style={{ margin: 0 }} className=" text-[11px] ">
-                {cardData.appVersions}
-              </p>
-
-              <div className="flex items-center gap-2 ">
-                <p style={{ margin: 0 }} className=" text-[11px]">
-                  dos apps
-                </p>
-                <img src="/word.svg" className="w-4 h-6" alt="Word" />
-                <img src="/excel.svg" className="w-4 h-6" alt="Excel" />
-                <img
-                  src="/power-point.svg"
-                  className="w-4 h-6"
-                  alt="PowerPoint"
-                />
-                <img src="/outlook.svg" className="w-4 h-6" alt="Outlook" />
-              </div>
-            </div>
-
-            <div className="flex flex-col items-start gap-1 mx-4">
-              <p style={{ margin: 0 }} className=" text-[20px] text-gray-900">
-                R${" "}
-                {selectedClientType === "cliente"
-                  ? cardData.priceClient
-                  : cardData.priceNonClient}{" "}
-                <span className="text-[20px] text-gray-600">/mês</span>
-              </p>
-              <p className="text-[10px] text-gray-600">por usuário</p>
-            </div>
-
-            <Button
-              className="self-center"
-              style={{ width: "160px", height: "50px" }}
-              variant="solid"
-              size="large"
-              color="magenta"
-              onClick={() => (navigate("/choose-plan"), window.scrollTo(0, 0))}
-            >
-              Assinar plano
-            </Button>
-            <ConfigProvider
-              theme={{
-                token: {
-                  colorBgTextHover: "none",
-                  colorBgTextActive: "none",
-                  colorText: "#666666",
-                  fontSize: 10,
-                },
-              }}
-            >
-              <Button
-                type="text"
-                size="small"
-                className=" cursor-pointer "
-                onClick={() => toggleDetails(cardData.id)}
-              >
-                {expandedCard === cardData.id ? (
-                  <>
-                    Menos Detalhes <ChevronUp className="inline w-3 h-3" />
-                  </>
-                ) : (
-                  <>
-                    Mais Detalhes <ChevronDown className="inline w-3 h-3" />
-                  </>
-                )}
-              </Button>
-            </ConfigProvider>
-
-            {expandedCard === cardData.id && (
-              <div className=" p-4 text-start bg-[#f0f0f0]  rounded-sm border-t">
-                <div className="flex items-start flex-col gap-3 text-sm text-gray-700">
-                  {cardData.details.map((detail, index) => (
-                    <div key={index}>
-                      <p style={{ margin: 0 }} className="font-medium  mb-1">
-                        {detail.title}
-                      </p>
-                      {detail.subtitle && (
-                        <p
-                          style={{ margin: 0 }}
-                          className="text-gray-500 text-xs"
-                        >
-                          {detail.subtitle}
-                        </p>
-                      )}
-                    </div>
-                  ))}
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  };
-
   return (
     <>
       <div
@@ -362,7 +185,7 @@ export default function Cards() {
                   className="flex-shrink-0 w-[260px] "
                   style={{ scrollSnapAlign: "center" }}
                 >
-                  <Card cardData={cardData} />
+                  <CardLayout cardData={cardData} />
                 </div>
               ))}
             </div>
@@ -383,7 +206,7 @@ export default function Cards() {
           {/* DESKTOP */}
           <div className="hidden lg:flex py-8 gap-4">
             {cardsData.map((cardData) => (
-              <Card key={cardData.id} cardData={cardData} />
+              <CardLayout key={cardData.id} cardData={cardData} />
             ))}
           </div>
         </div>
