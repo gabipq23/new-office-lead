354c1d2afe2d1d4f19ce3c3d01dd09ce2ee9969e
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2025-11-19 10:15:13 -0300

    feat: add formatting utilities for CEP, CNPJ, CPF, and phone numbers

--- src/interfaces/order.ts
+++ src/interfaces/order.ts
@@ -10,26 +10,35 @@ export interface Plan {
 }
 
 export interface OrderData {
-  id?: number;
-  email: string;
-  domainName: string;
-  cnpj: string;
-  managerPhone: string;
-  status?: string | null;
-  status_pos_venda?: string | null;
-  id_vivo_corp?: string | null;
-  consultor_responsavel?: string | null;
-  equipe?: string | null;
-  id_crm?: string | null;
-  obs_consultor?: string | null;
-  manager_name?: string | null;
-  created_at?: string;
-  ordernumber?: string;
-  isVivoClient: boolean | number;
-  alreadyHaveMicrosoftDomain: boolean | number;
   acceptContact: boolean | number;
   acceptTerms: boolean | number;
+  alreadyHaveMicrosoftDomain: boolean | number;
+  client_ip?: string | null;
+  cnpj: string;
+  company_name?: string | null;
+  consultor_responsavel?: string | null;
+  cpf?: string | null;
+  created_at?: string;
+  credito_servicos?: string | null;
+  domainName: string;
+  email: string;
+  equipe?: string | null;
+  finger_print?: string | null;
+  i_have_authorization?: boolean;
+  id?: number;
+  id_crm?: string | null;
+  id_vivo_corp?: string | null;
+  isVivoClient: boolean | number;
+  managerPhone: string;
+  manager_name?: string | null;
+  obs_consultor?: string | null;
+  observacao_consultor?: string;
+  ordernumber?: string;
   plans: Plan[];
+  second_manager_phone?: string;
+  status?: string | null;
+  status_pos_venda?: string | null;
+  url?: string | null;
 }
 
 export interface CreateOrderData {
@@ -44,6 +53,13 @@ export interface CreateOrderData {
     domainName: string;
     acceptTerms: boolean;
     status: string;
+    i_have_authorization?: boolean;
+    second_manager_phone?: string;
+    cpf?: string | null;
+    client_ip?: string | null;
+    url?: string | null;
+    finger_print?: string | null;
+    company_name?: string | null;
   };
   itens: Array<{
     planName: string;
